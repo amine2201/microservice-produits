d7cbfa43469240021818c936a957b24dd622fe38
Amine Jaoui <aminejaoui01@gmail.com>
2023-12-31 14:50:16 +0100

    adding configuration class

--- src/main/java/com/jaoui/microserviceproduits/dao/ProductRepository.java
+++ src/main/java/com/jaoui/microserviceproduits/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.jaoui.microserviceproduits.dao;
 
 import com.jaoui.microserviceproduits.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
